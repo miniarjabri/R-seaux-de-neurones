3c8be6e38e78207461bb52cc178871c3167eaef3
miniar <123066240+miniarjabri@users.noreply.github.com>
2024-04-22 17:49:58 +0200

    Update Echantillon.java

--- Echantillon.java
+++ Echantillon.java
@@ -1,18 +1,19 @@
 package ET;
 
-public class Echantillon {
-	    private final boolean x;
-	    private final boolean y;
-	    private final boolean output; // Expected output for the AND operation
+public class Echantillon extends ReseauNeurone {
+    private float[] input;
+    private float[] output;
 
-	    public Echantillon(boolean x, boolean y, boolean output) {
-	        this.x = x;
-	        this.y = y;
-	        this.output = output;
-	    }
+    public Echantillon(float[] input, float[] output) {
+        this.input = input;
+        this.output = output;
+    }
 
-	    // Getters here...
-	    public boolean getX() { return x; }
-	    public boolean getY() { return y; }
-	    public boolean getOutput() { return output; }
+    public float[] getInput() {
+        return input;
+    }
+
+    public float[] getOutput() {
+        return output;
+    }
 }
