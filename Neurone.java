c443218d337ec700282531fb5903cb533857c9fc
miniar <123066240+miniarjabri@users.noreply.github.com>
2024-05-17 08:30:45 +0200

    Delete Neurone.java

--- Neurone.java
+++
@@ -1,8 +0,0 @@
-package réseaux_de_neurones;
-import java.util.Random;
-
-
-public abstract class Neurone {
-    // Méthode abstraite pour calculer les valeurs à envoyer à chaque neurone de la couche suivante
-    public abstract double Factivation(double somme,String fonction);
-}
