840fd30c7ff7906656f0418e60b9d08e8e5e90c6
miniar <123066240+miniarjabri@users.noreply.github.com>
2024-05-24 08:08:16 +0200

    Update Couche.java

--- etape3-1/R-seaux-de-neurones-main/LesCouches/Couche.java
+++ etape3-1/R-seaux-de-neurones-main/LesCouches/Couche.java
@@ -7,46 +7,27 @@ public interface Couche<N> {
     // Méthode pour obtenir le type de la couche (couche d'entrée, cachée ou de sortie)
     String getTypeCouche();
 
+    // Méthode pour obtenir les neurones de la couche
     N[] getNeurones();
 
+    // Méthode pour obtenir la sortie des neurones de la couche
     double[] getSortie();
 
+    // Méthode pour mettre à jour les biais et les poids des neurones de la couche
     void updateBiasesAndWeights(double[] nablaB, double[][] nablaW);
 
+    // Méthode pour obtenir les poids des connexions des neurones de la couche
     double[][] getPoids();
 
+    // Méthode pour obtenir les activations des neurones de la couche
     double[] activation();
 
+    // Méthode pour obtenir les valeurs Z (somme pondérée plus biais) des neurones de la couche
     double[] getZ(double[] activation);
 
+    // Méthode pour obtenir la dérivée de l'activation des neurones de la couche
     double[] activationPrime(double[] ds);
 
+    // Méthode pour obtenir la sortie des neurones de la couche en fonction des activations
     double[] sortie(double[] activations);
 }
-
-/*package LesCouches;
-
-public interface Couche<N> {
-    // Méthode pour obtenir le nombre de neurones dans la couche
-    int getNombreNeurones();
-
-    // Méthode pour obtenir le type de la couche (couche d'entrée, cachée ou de sortie)
-    String getTypeCouche();
-
-    N[] getNeurones();
-
-    double[] getSortie();
-
-    void updateBiasesAndWeights(double[] nablaB, double[][] nablaW);
-
-    double[][] getPoids();
-
-    double[] activation();
-
-    double[] getZ(double[] activation);
-
-    double[] activationPrime(double[] ds);
-
-    double[] sortie(double[] activations);
-}
-*/
